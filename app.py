dba3894c3b0abd05091a2fd046e30c7abbdd2ab3
ankitmishralive <=ankitmishra.letter@gmail.com>
2024-05-31 03:53:29 +0530

    br

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 
 
 from func import doAddition,doSubtraction
+from mulfunc import doMul
 
 def main():
     print('Welcome to demo app')
     print(""" \n Select the function from below option\n
           1. Add 
           2. Subtract
+          3. Multiply 
           """)
 
     user_input = input('Select the function')
@@ -17,8 +19,10 @@ def main():
         result = doAddition(a,b)
     elif user_input == '2':
         result = doSubtraction(a,b)
+    elif user_input == '3':
+        result = doMul(a,b)
     
     print('Result ',result)
 
-
-main() 
+if __name__ == '__main__':
+    main() 
---
+++ mulfunc.py
@@ -0,0 +1,5 @@
+
+
+def doMul(a,b):
+    return a*b 
+
